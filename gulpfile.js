bd343d915db2e5e7fe4dddfd6e27c7ddab7637a9
Stephen Owen <stephenowen682@yahoo.com>
2018-02-04 17:51:05 +0800

    Add gulp-sass & browser-sync packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
 var gulp = require("gulp");
+var browserSync = require("browser-sync");
+
+gulp.task("styles", function() {});
+
+gulp.task("scripts", function() {});
+
+gulp.task("html", function() {});
