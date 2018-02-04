6c02df365946c8f7755e17db722e37caea36b89b
Stephen Owen <stephenowen682@yahoo.com>
2018-02-04 20:18:35 +0800

    Add gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 var gulp = require("gulp");
 var browserSync = require("browser-sync");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
 
-gulp.task("styles", function() {});
+// File paths
+var assetsScss = "src/assets/scss/**/*.scss";
+
+var tempCss = "src/temp/styles";
+// Styles
+gulp.task("styles", function() {
+  return gulp
+    .src(assetsScss)
+    .pipe(sass())
+    .on("error", function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit("end");
+    })
+    .pipe(autoprefixer("last 2 versions"))
+    .pipe(gulp.dest(tempCss));
+});
 
 gulp.task("scripts", function() {});
 
